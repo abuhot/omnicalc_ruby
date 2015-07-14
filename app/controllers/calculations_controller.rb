0f70de3faaacaacbd3edb3d4570283e648e126ab
abuhot <abuhot@chicagobooth.edu>
2015-07-13 22:01:34 -0500

    HW 3 Changes

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,19 +11,24 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.strip.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.delete(' ').length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split.count
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text.downcase.gsub("."," ").split.count(@special_word.downcase)
+
   end
 
   def loan_payment
     @apr = params[:annual_percentage_rate].to_f
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
+    # c is the monthly interest rate
+    c = ((@apr/100)/12).to_f
+    n = @years * 12
+
 
     # ================================================================================
     # Your code goes below.
@@ -32,7 +37,11 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    #Monthly Payment Formula - P = L[c(1 + c)n]/[(1 + c)n - 1]
+
+    @monthly_payment = @principal*(c*(1+c)**n)/((1+c)**n - 1)
+
+    #@monthly_payment = @principal.to_s + " " + c.to_s + " " + n.to_s
   end
 
   def time_between
@@ -48,12 +57,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @days/365
   end
 
   def descriptive_statistics
@@ -64,26 +73,89 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @numbers.max - @numbers.min
 
-    @median = "Replace this string with your answer."
+   # @median = "Replace this string with your answer."
 
-    @sum = "Replace this string with your answer."
+    @sum = @numbers.inject(0) { |result, element| result + element }
 
-    @mean = "Replace this string with your answer."
+    #Determine Median
+        #First determine even or odd number in array
+         if @count % 2 == 0
+            even = true
+         end
 
-    @variance = "Replace this string with your answer."
+    if even
+        m_pos = (@count/2) - 1
+        @median = ((@sorted_numbers[m_pos] + @sorted_numbers[(m_pos + 1)])/2)
+        #@median = @sorted_numbers[1]
+    else
+        m_pos = (@count - 1)/2
+        @median = @sorted_numbers[m_pos]
+        #@median = @sorted_numbers[1]
+    end
 
-    @standard_deviation = "Replace this string with your answer."
 
-    @mode = "Replace this string with your answer."
+    @mean = @sum/@count
+
+    var = 0
+
+    @numbers.each do |i|
+       #var = var.to_f + ( ( (i - @mean.to_f) ** 2)/(@count))
+       var = var.to_f + ( (i - @mean.to_f) ** 2)
+    end
+
+    var = var/@count
+    @variance = var.to_f
+
+    #@standard_deviation = @variance.math.sqrt
+
+    @standard_deviation = Math.sqrt(@variance)
+
+    #calculate mode
+        #initialize variables
+        i = 0
+        arrayCount = 0
+        last_sorted_number = @minimum
+        h = {}
+        #First cycle throught the sorted array and build a hash that contains the number of occurrences for each number.  They key will be the number
+        @sorted_numbers.each do |v_num|
+
+            arrayCount = arrayCount + 1
+
+           if v_num == last_sorted_number
+                i = i + 1
+           else
+                h["#{last_sorted_number}"] = i
+                i = 1
+
+           end
+
+           last_sorted_number = v_num.to_i
+
+           if arrayCount = @count
+
+               h["#{last_sorted_number}"] = i
+
+          end
+
+        end
+
+       max_key = h.values.max
+       mode = h.select { |k, v| v == max_key }.keys
+       @mode = mode[0].to_f
+
+
+
+
+    #@mode = "Replace this string with your answer."
   end
 end
